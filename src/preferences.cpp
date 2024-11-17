75dec2e8feeaf9d6a98ccdfa275bd0ba6e539481
op200 <112297583+op200@users.noreply.github.com>
2024-11-18 03:32:46 +0800

    Update preferences.cpp: Debug: var's name error
    _("Log level") -> _("Log Level")

--- src/preferences.cpp
+++ src/preferences.cpp
@@ -503,7 +503,7 @@ void VapourSynth(wxTreebook *book, Preferences *parent) {
 
 	const wxString log_levels[] = { "Quiet", "Fatal", "Critical", "Warning", "Information", "Debug" };
 	wxArrayString log_levels_choice(6, log_levels);
-	p->OptionChoice(general, _("Log level"), log_levels_choice, "Provider/Video/VapourSynth/Log Level");
+	p->OptionChoice(general, _("Log Level"), log_levels_choice, "Provider/Video/VapourSynth/Log Level");
 	p->CellSkip(general);
 	p->OptionAdd(general, _("Load user plugins"), "Provider/VapourSynth/Autoload User Plugins");
 
